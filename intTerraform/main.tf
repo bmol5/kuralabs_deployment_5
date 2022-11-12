7493638d43498909493047a32cf860e97d2d7afc
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-11-12 00:31:20 -0500

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -30,7 +30,7 @@ resource "aws_ecs_task_definition" "aws-ecs-task" {
   [
   {
       "name": "url-container",
-      "image": "tsanderson77/classact:latest",
+      "image": "bmol5/flask:latest",
       "logConfiguration": {
         "logDriver": "awslogs",
         "options": {
